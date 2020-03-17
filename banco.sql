e733c342eacb5d3ba25a5f813473c06bb0ce327e
victorMenegass0 <53906166+victorMenegass0@users.noreply.github.com>
2020-03-16 21:26:45 -0300

    arrumei o banco que aparentemente tava errado

--- banco.sql
+++ banco.sql
@@ -15,38 +15,39 @@ url_website varchar(100),
 ds_endereco varchar(150)
 );
 
-create table tb_formaçao(
-cd_formaçao int(6)not null auto_increment primary key,
-nm_instituiçao varchar(155)not null,
-dt_anoini int(4)not null,
-dt_anofim int(4)not null,
-nm_area varchar(70)not null,
+create table tb_formacao(
+cd_formacao int(6)not null auto_increment primary key,
+nm_curso varchar(12)not null,
+nm_instituicao varchar(155)not null,
+dt_anoini varchar(4)not null,
+dt_anofim varchar(4)not null,
+ds_opcional varchar(130) not null, 
 id_pessoa int(5)not null, 
 foreign key(id_pessoa)references tb_pessoa(cd_pessoa) 
 );
 
-create table experiencia(
-cd_experiencia int(6)auto_increment primary key not null,
-nm_serviço varchar(45)not null,
-nm_area varchar(25)not null,
-ds_descriçao varchar(255)not null, 
+create table tb_exp(
+cd_exp int(6)auto_increment primary key not null,
+nm_servico varchar(45)not null,
+nm_empresa varchar(25)not null,
+ds_descricao varchar(255)not null, 
 dt_anoini int(4)not null,
 dt_anofim int(4)not null,
 id_pessoa int(5)not null, 
 foreign key(id_pessoa)references tb_pessoa(cd_pessoa) 
 );
-create table referencias(
-cd_referencias int(6)auto_increment primary key not null,
-nm_referencia varchar(80)not null, 
+create table tb_ref(
+cd_ref int(6)auto_increment primary key not null,
+nm_ref varchar(80)not null, 
 nm_profissao varchar(80)not null,
 nr_telefone varchar(20)not null,
 id_pessoa int(5)not null, 
 foreign key(id_pessoa)references tb_pessoa(cd_pessoa)
 );
-create table tb_habilidades(
-cd_habilidade int(6)auto_increment primary key not null,
-nm_habilidade varchar(45)not null, 
-qt_habilidade int(3)not null,
+create table tb_hab(
+cd_hab int(6)auto_increment primary key not null,
+nm_hab varchar(45)not null, 
+qt_hab int(3)not null,
 id_pessoa int(5)not null, 
 foreign key(id_pessoa)references tb_pessoa(cd_pessoa)
-);
+);   
